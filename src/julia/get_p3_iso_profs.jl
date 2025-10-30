1df31be7caf09acf49fb87ddea3bcb95373a1e40
Simon de Szoeke <simon.deszoeke@oregonstate.edu>
2025-10-30 14:19:11 -0700

    add julia/get_p3_iso_profs.jl

---
+++ src/julia/get_p3_iso_profs.jl
@@ -0,0 +1,7 @@
+using NCDatasets
+using HTTP
+
+url = "https://www.ncei.noaa.gov/thredds-ocean/catalog/psl/atomic/p3/Picarro/catalog.html" # thredds
+url = "https://www.ncei.noaa.gov/data/oceans/oar/psl/atomic-2020/p3/Picarro/"
+# ds = NCDataset(url)
+
